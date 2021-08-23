5ab3eefd1c10e3b6a91e08846df33e261c5da367
shabahat97 <shabahatalichishti@gmail.com>
2021-08-23 16:31:01 +0500

    Add project files.

---
+++ Differencebetween$scopeand$rootscope/Script.js
@@ -0,0 +1,12 @@
+﻿var app = angular
+    .module("Demo", [])
+    .controller("redColourController", function ($scope, $rootScope) {
+        $scope.redColour = "I am red Colour";
+        $rootScope.rootScopeColour = "I am root scope controller";
+    })
+    .controller("greenColourController", function ($scope, $rootScope) {
+        $scope.greenColour = "I am green Colour";
+        $rootScope.rootScopeColour = "I am root scope controller";
+    });
+
+
